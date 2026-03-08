a5f3755baab653d073630e21df2de4e2348094da
frierensaan <frierensaan@gmail.com>
2026-03-08 16:38:27 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from mt6768-common
-$(call inherit-product, device/xiaomi/mt6768-common/mt6768.mk)
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
@@ -15,9 +15,29 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    fstab.mt6768 \
+    fstab.mt6768.ramdisk \
+    init.recovery.mt6768.rc \
+    init.mt6768.rc \
+    init.mt6768.usb.rc
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek \
+    hardware/xiaomi
 
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 29
