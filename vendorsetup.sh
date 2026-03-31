aa85be1a2e0c58a81697a8b08f12a5d0eb4e3a9e
frierensaan <frierensaan@gmail.com>
2026-03-31 21:45:53 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-export FOX_BUILD_DEVICE="lancelot"
+
+# OrangeFox Build Variables for Xiaomi Redmi 9 (lancelot)
 export OF_MAINTAINER="frieren"
+export FOX_VERSION="R11.1"
 export FOX_BUILD_TYPE="Unofficial"
-export OF_DEFAULT_KEYMASTER_VERSION=4.0
-export OF_INCLUDE_FBE_METADATA_DECRYPT=1
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+
+# Crypto/Decryption (Diagnostic bypass to isolate UI hangs)
+export OF_SKIP_FBE_DECRYPTION=1
+
+# Screen Settings (Lancelot specific)
 export OF_SCREEN_H=2340
 export OF_STATUS_H=80
-export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
-export TARGET_DEVICE_ALT="shiva"
-export FOX_TARGET_DEVICES="lancelot,shiva"
-export OF_SKIP_FBE_DECRYPTION=1
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_HIDE_NOTCH=1
+export OF_CLOCK_POS=1
+
+# Hardware/Misc
+export OF_USE_GREEN_LED=0
+export FOX_ENABLE_APP_MANAGER=1
+export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
 
